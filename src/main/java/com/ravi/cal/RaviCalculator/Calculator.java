39d4c45694109e6b4512f2b7240961319ab8bf90
Chaitu240 <140746773+Chaitu240@users.noreply.github.com>
2023-10-27 12:16:18 +0530

    Update Calculator.java

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -23,10 +23,6 @@ public class Calculator {
 		return first+second;
 	}
 	
-	public long mulFucn(long first, long second){
-		
-		return first*second;
-	}
 	public long subFucn(long first, long second){
 		
 		return second-first;
